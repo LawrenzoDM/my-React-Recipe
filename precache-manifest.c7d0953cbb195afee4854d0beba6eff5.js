64051d4a3f69b517ec4caf4511ca9eee0d159524
LawrenzoDM <lawrenzodelmc@outlook.com>
2020-11-26 10:03:33 +0100

    Updates

---
+++ precache-manifest.c7d0953cbb195afee4854d0beba6eff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d020583c1e9b5e5a97693aa2456e7ad3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "667aa5ba684ad29900ba",
+    "url": "./static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "667aa5ba684ad29900ba",
+    "url": "./static/js/2.b6d430a9.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.b6d430a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d036c01d13155ef546",
+    "url": "./static/js/main.27f395db.chunk.js"
+  },
+  {
+    "revision": "8cdb25f3ffb41440c4d8",
+    "url": "./static/js/runtime-main.e6c4d886.js"
+  }
+]);
